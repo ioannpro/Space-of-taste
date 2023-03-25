68c61fcc060dd6da405710f34fdac89e3ac639f4
ioannpro <ioann0602@gmail.com>
2023-03-25 21:46:29 +0200

    Create logging system in service

--- DBBackuper/log.py
+++ DBBackuper/log.py
@@ -4,38 +4,65 @@ import logging
 
 # Setup
 
-name = 'DBBackuper'
-logging.basicConfig(level=logging.INFO )
+FORMAT = '[%(asctime)s] %(name)s %(levelname)s: %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
 # Create Handler
 
-terminal = logging.StreamHandler()
-debug_handler = logging.FileHandler(filename=f'{log_dir}/debug.log', mode='w')
-warning_handler = logging.FileHandler(filename=f'{log_dir}/warning.log', mode='w')
-error_handler = logging.FileHandler(filename=f'{log_dir}/error.log', mode='w')
-format = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+debug_handler = logging.FileHandler(filename=f'{log_dir}/debug.log', mode='a')
+warning_handler = logging.FileHandler(filename=f'{log_dir}/warning.log', mode='a')
+error_handler = logging.FileHandler(filename=f'{log_dir}/error.log', mode='a')
 
-# Set Format to Handler
+# Creater Formatter
 
-terminal.setFormatter(format)
-debug_handler.setFormatter(format)
-warning_handler.setFormatter(format)
-error_handler.setFormatter(format)
+formatter_a = logging.Formatter(FORMAT)
+
+# Create Filter
+
+class DebugFilter(logging.Filter):
+    def filter(self, record):
+        if record.levelname == "DEBUG":
+            return True
+        
+class WarningFilter(logging.Filter):
+    def filter(self, record):
+        if record.levelname == "WARNING":
+            return True
+        
+class ErrorFilter(logging.Filter):
+    def filter(self, record):
+        if record.levelname == "ERROR":
+            return True
+    
+# Set Formatter
+
+debug_handler.setFormatter(formatter_a)
+warning_handler.setFormatter(formatter_a)
+error_handler.setFormatter(formatter_a)
+
+# Add Filter
+
+debug_handler.addFilter(DebugFilter())
+warning_handler.addFilter(WarningFilter())
+error_handler.addFilter(ErrorFilter())
 
 # Create Logging Object
 
-logger = logging.getLogger(name)
+logger = logging.getLogger(__name__)
+
+# Add Handler
+
+logger.addHandler(debug_handler)
+logger.addHandler(warning_handler)
+logger.addHandler(error_handler)
 
 
-logger.addHandler(terminal)
 
-
-
-
-logger.warning('Protocol problem: %s', 'connection reset')
 
 def main():
-    
+    logger.debug('BuuuuuuuuuG')
+    logger.warning('Protocol problem: %s', 'connection reset')
+
     logger.info('Started')
     logger.info('Finished')
 
