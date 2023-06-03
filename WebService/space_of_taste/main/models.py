6cc5facb398d1212f61bef42734cab9a61894f2b
Stop2234 <egorik7892005@gmail.com>
2023-06-03 23:32:32 +0300

    Stop

--- WebService/space_of_taste/main/models.py
+++ WebService/space_of_taste/main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Dish(models.Model):
+    object=None
+    name=models.CharField(max_length=320,verbose_name='Имя')
+    image=models.ForeignKey('File')
+    categori=models.ForeignKey('Category')
+    description=models.TextField(blank=True)
+    options=models.JSONField()
+    def __str__(self):
+        return self.name
+class Category(models.Model):
+    name=models.CharField(max_length=320)
+    upcategory=models.SmallIntegerField()
+    icons=models.ForeignKey('File')
+
+class File(models.Model):
+    url=models.TextField(blank=True)
+
+
+
+
+
+
